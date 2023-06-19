6238e0b33fb54c61d9fbb34530d3abef65eef46e
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-06-19 19:20:41 +0200

    Refactor component Modal to functional component. Use useCallback to memoize functions between re-renders

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,45 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 
 import css from './Modal.module.css';
+import { useCallback, useEffect } from 'react';
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleEscKeyPress);
-  }
+const Modal = ({ modalImage, alt, onClose }) => {
+  const handleEscKeyPress = useCallback(
+    e => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleEscKeyPress);
-  }
+  const handleOverlayClick = useCallback(
+    e => {
+      if (e.target === e.currentTarget) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
-  handleEscKeyPress = e => {
-    if (e.key === 'Escape') {
-      this.props.onClose();
-    }
-  };
+  useEffect(() => {
+    document.addEventListener('keydown', handleEscKeyPress);
+  }, [handleEscKeyPress]);
 
-  handleOverlayClick = e => {
-    if (e.target === e.currentTarget) {
-      this.props.onClose();
-    }
-  };
+  useEffect(() => {
+    return () => {
+      document.removeEventListener('keydown', handleEscKeyPress);
+    };
+  }, [handleEscKeyPress]);
 
-  render() {
-    const { modalImage, alt } = this.props;
-    return (
-      <div className={css.overlay} onClick={this.handleOverlayClick}>
-        <div className={css.modal}>
-          <img src={modalImage} alt={alt} />
-        </div>
+  return (
+    <div className={css.overlay} onClick={handleOverlayClick}>
+      <div className={css.modal}>
+        <img src={modalImage} alt={alt} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   modalImage: PropTypes.string,
