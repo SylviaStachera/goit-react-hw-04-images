2cc62ec4643551612fe7296965a2becc62ece2bb
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-06-19 18:55:11 +0200

    Refactor component ImageGallery to functional component

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,28 +3,22 @@ import PropTypes from 'prop-types';
 
 import css from './ImageGallery.module.css';
 
-import { Component } from 'react';
-
-class ImageGallery extends Component {
-  render() {
-    const { images, modalImageUrl } = this.props;
-
-    return (
-      <>
-        <ul className={css.imageGallery}>
-          {images.map(({ id, tags, webformatURL, largeImageURL }) => (
-            <ImageGalleryItem
-              key={id}
-              tags={tags}
-              smallImageUrl={webformatURL}
-              modalImageUrl={() => modalImageUrl(largeImageURL, tags)}
-            />
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
+const ImageGallery = ({ images, modalImageUrl }) => {
+  return (
+    <>
+      <ul className={css.imageGallery}>
+        {images.map(({ id, tags, webformatURL, largeImageURL }) => (
+          <ImageGalleryItem
+            key={id}
+            tags={tags}
+            smallImageUrl={webformatURL}
+            modalImageUrl={() => modalImageUrl(largeImageURL, tags)}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
   images: PropTypes.array,
