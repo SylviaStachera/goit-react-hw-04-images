de4f0d9fa319fb2cce2fd5ecc573c7979ce29d18
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-06-19 18:58:01 +0200

    Refactor component Loader to functional component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,25 +1,22 @@
 import { ThreeDots } from 'react-loader-spinner';
-import { Component } from 'react';
 
 import css from './Loader.module.css';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className={css.loader}>
-        <ThreeDots
-          height="150"
-          width="150"
-          radius="9"
-          color="#3f51b5"
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{}}
-          wrapperClassName=""
-          visible={true}
-        />
-      </div>
-    );
-  }
-}
+const Loader = () => {
+  return (
+    <div className={css.loader}>
+      <ThreeDots
+        height="150"
+        width="150"
+        radius="9"
+        color="#3f51b5"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    </div>
+  );
+};
 
 export default Loader;
